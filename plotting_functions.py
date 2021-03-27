ae2b6b4ad6f6837876934602328a1e44e603c9a6
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-03-27 12:33:31 +0000

    fonrontier plot method

---
+++ plotting_functions.py
@@ -0,0 +1,18 @@
+from typing import List, Union
+
+import matplotlib.pyplot as plt
+from f1_objects import Team
+
+
+def plot_frontier(teams: List[Team], save_path: Union[str, None]):
+    fig = plt.figure()
+    prices = [team["price"] for team in teams]
+    points = [team["points"] for team in teams]
+    plt.scatter(prices, points)
+    plt.xlabel("price")
+    plt.ylabel("points")
+    plt.plot([100, 100], [0.9 * min(points), 1.1 * max(points)], linestyle="dashed")
+    fig.show()
+
+    if save_path is not None:
+        fig.savefig(save_path, dpi=100)
