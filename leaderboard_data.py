c3158f8c615b78704570367479377c3c9cbaabad
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-03-27 12:33:19 +0000

    intiial commit of leaderboard projections according to odds

---
+++ leaderboard_data.py
@@ -0,0 +1,25 @@
+import constants
+import drivers
+
+LEADERBOARD = {
+    constants.VERSTAPPEN: drivers.verstappen,
+    constants.HAMILTON: drivers.hamilton,
+    constants.BOTTAS: drivers.bottas,
+    constants.PEREZ: drivers.perez,
+    constants.NORRIS: drivers.norris,
+    constants.RICCIARDO: drivers.ricciardo,
+    constants.LECLERC: drivers.leclerc,
+    constants.SAINZ: drivers.sainz,
+    constants.GASLY: drivers.gasly,
+    constants.TSUNODA: drivers.tsunoda,
+    constants.STROLL: drivers.stroll,
+    constants.ALONSO: drivers.alonso,
+    constants.VETTEL: drivers.vettel,
+    constants.OCON: drivers.ocon,
+    constants.RAIKKONEN: drivers.raikkonen,
+    constants.GIOVINAZZI: drivers.giovinazzi,
+    constants.RUSSELL: drivers.russell,
+    constants.MAZEPIN: drivers.mazepin,
+    constants.LATIFI: drivers.latifi,
+    constants.SCHUMACHER: drivers.schumacher,
+}
