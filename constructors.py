6aa629b8ff87d9a4c425b162f6ea20f608b616dd
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-04-18 21:02:09 +0100

    updated constructor prices

--- constructors.py
+++ constructors.py
@@ -2,27 +2,27 @@ import constants
 from f1_objects import Constructor
 
 mercedes = Constructor(
-    name=constants.MERCEDES, price=38.0, drivers=[constants.HAMILTON, constants.BOTTAS]
+    name=constants.MERCEDES, price=37.8, drivers=[constants.HAMILTON, constants.BOTTAS]
 )
 red_bull = Constructor(
-    name=constants.RED_BULL, price=25.9, drivers=[constants.VERSTAPPEN, constants.PEREZ]
+    name=constants.RED_BULL, price=26.0, drivers=[constants.VERSTAPPEN, constants.PEREZ]
 )
 mclaren = Constructor(
-    name=constants.MCLAREN, price=18.9, drivers=[constants.NORRIS, constants.RICCIARDO]
+    name=constants.MCLAREN, price=18.8, drivers=[constants.NORRIS, constants.RICCIARDO]
 )
 ferrari = Constructor(
-    name=constants.FERRARI, price=18.1, drivers=[constants.LECLERC, constants.SAINZ]
+    name=constants.FERRARI, price=18.5, drivers=[constants.LECLERC, constants.SAINZ]
 )
 alpha_tauri = Constructor(
-    name=constants.ALPHA_TAURI, price=12.7, drivers=[constants.TSUNODA, constants.GASLY]
+    name=constants.ALPHA_TAURI, price=13.1, drivers=[constants.TSUNODA, constants.GASLY]
 )
 aston_martin = Constructor(
     name=constants.ASTON_MARTIN,
-    price=17.6,
+    price=17.0,
     drivers=[constants.STROLL, constants.VETTEL],
 )
 alpine = Constructor(
-    name=constants.ALPINE, price=15.4, drivers=[constants.ALONSO, constants.OCON]
+    name=constants.ALPINE, price=15.1, drivers=[constants.ALONSO, constants.OCON]
 )
 alfa_romeo = Constructor(
     name=constants.ALFA_ROMEO,
