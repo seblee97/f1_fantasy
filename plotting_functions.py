9463471def73fcaa6e02e75e6fbb5701aba25ade
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2021-04-18 21:03:06 +0100

    label

--- plotting_functions.py
+++ plotting_functions.py
@@ -8,10 +8,17 @@ def plot_frontier(teams: List[Team], save_path: Union[str, None]):
     fig = plt.figure()
     prices = [team["price"] for team in teams]
     points = [team["points"] for team in teams]
-    plt.scatter(prices, points)
+    plt.scatter(prices, points, label="random team samples")
     plt.xlabel("price")
     plt.ylabel("points")
-    plt.plot([100, 100], [0.9 * min(points), 1.1 * max(points)], linestyle="dashed")
+    plt.plot(
+        [100, 100],
+        [0.9 * min(points), 1.1 * max(points)],
+        linestyle="dashed",
+        color="red",
+        label="budget",
+    )
+    plt.legend()
     fig.show()
 
     if save_path is not None:
